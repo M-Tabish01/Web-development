e957d043df54fe71175f3d31990667f1cc9b8147
M-Tabish01 <tabish98tk027123@gmail.com>
2025-12-25 00:35:06 +0500

    Add songs and more

--- Tutorial 83/script.js
+++ Tutorial 83/script.js
@@ -10,17 +10,41 @@ async function getsongs() {
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if(element.href.endsWith(".mp3")){
-            songs.push(element.href)
+            songs.push(element.href.split("%5Csongs%5C")[1])
         }
-    }
+    } 
     return songs
 }
 async function main() {
     let songs = await getsongs();
     console.log(songs)
     
+    let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0];
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li> 
+        
+                            <img class="invert" src="img/music.svg" alt="">
+                            <div class="info">
+                                <div>${song.replaceAll("%20", " ")} </div>
+                            </div>
+                            <div class="playnow">
+                                <span>Play Now</span>
+                            </div>
+                            <img class="invert" src="img/play.svg" alt="">
+                        
+        
+        
+        
+        
+        </li>`
+    }
+
     var audio = new Audio(songs[0])
-    audio.play()
+    // audio.play()
+
+    audio.addEventListener("loadeddata", ()=>{
+        console.log(audio.duration, audio.currentSrc, audio.currentTime)
+    });
 }
 
 main()
